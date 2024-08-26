fe748ed7ca1501d0970004bd42327bbd14cdfcc1
Eric <etrebinofigueroa@alu.frlp.utn.edu.ar>
2024-08-26 20:31:41 -0300

    pruebaPrueba

---
+++ prueba/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+                  <br /><br />       Proximamente...
+        </p>
+        <a
+          className="App-link"
+          href="https://trello.com/b/HVtWgmNz/utng032024"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Trello
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
