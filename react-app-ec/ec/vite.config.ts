86e264f6a9e923cd14468a82e14527ab498534ba
FrancoPetosa99 <franco.petosa15@gmail.com>
2024-09-16 10:04:50 -0300

    listar comandos en DockerFile

--- react-app-ec/ec/vite.config.ts
+++ react-app-ec/ec/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Asegura que la aplicación sea accesible externamente
+    port: 4173,      // Usa el mismo puerto que expones en Docker
+  }
+});
