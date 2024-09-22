a8c0549bb0fe747ebb3a6c810bdc3bbf147cd7f5
FrancoPetosa99 <franco.petosa15@gmail.com>
2024-09-22 20:30:51 -0300

    easychoice-api-rest

--- springboot-api-ec/src/main/java/com/api/easychoice/controller/ProfesorController.java
+++ springboot-api-ec/src/main/java/com/api/easychoice/controller/ProfesorController.java
@@ -1,8 +1,21 @@
 package com.api.easychoice.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RequestMapping
+@RestController
+@RequestMapping("/api/profesores")
 public class ProfesorController {
     
+    @GetMapping
+    public String getProfesores() {
+        return "Lista de profesores";
+    }
+
+    @GetMapping("/{id}")
+    public String getProfesorById() {
+
+        return "Lista de profesores";
+    }
 }
