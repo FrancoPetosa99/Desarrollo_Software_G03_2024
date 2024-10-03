11c3e79a549009e1c01e5a2ddd0fdaaa84a3bb0d
FrancoPetosa99 <franco.petosa15@gmail.com>
2024-10-03 00:41:18 -0300

    login

--- springboot-api-ec/src/main/java/com/api/easychoice/controller/AuthController.java
+++ springboot-api-ec/src/main/java/com/api/easychoice/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.api.easychoice.controller;
 
 import com.api.easychoice.dto.AuthDTO;
-import com.api.easychoice.response.ErrorResponse;
-import com.api.easychoice.response.ResponseHttp;
-import com.api.easychoice.response.SuccessResponse;
+import com.api.easychoice.exception.BadRequestException;
+import com.api.easychoice.exception.NotFoundException;
+import com.api.easychoice.exception.UnauthorizedException;
+import com.api.easychoice.response.HttpBodyResponse;
+import com.api.easychoice.response.ResponseFactory;
 import com.api.easychoice.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +19,43 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private ResponseFactory responseFactory;
+
     @PostMapping("/credentials")
     public ResponseEntity<Object> loginByCredentials(@RequestBody AuthDTO authDTO) {
 
+        String email = authDTO.getEmail();
+        String password = authDTO.getPassword();
         
+        try {
+            
+
+            if (email == null || password == null) throw new BadRequestException();
+    
+            String profesorId = authService.loginByCredentials(email, password);
 
 
-        // devolver respuesta al cliente
-        return ResponseEntity
-        .status(201)
-        .body("Token");
+            HttpBodyResponse data = new HttpBodyResponse
+                                    .Builder()
+                                    .message("usuario autenticado")
+                                    .userFriendlyMessage("login exitoso")
+                                    .data(profesorId)
+                                    .build();
+
+            // devolver respuesta al cliente
+            return responseFactory.success(data);
+
+
+        } catch(BadRequestException e) {
+            return responseFactory.badRequest("Los datos proporcionados son inválidos. Por favor, revisa y vuelve a intentarlo");
+        } catch (NotFoundException e) {
+            return responseFactory.errorNotFound("No existe Profesor con email: " + email);
+        } catch(UnauthorizedException e){
+            return responseFactory.unauthorizedError();
+        } catch (Exception e) {
+            return responseFactory.internalServerError();
+        }
+
     }
 }
