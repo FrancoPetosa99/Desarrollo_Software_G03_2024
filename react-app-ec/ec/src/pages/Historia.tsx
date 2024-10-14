d85eaba4c0f07d432bf2933ded75be54f8793613
Eric <etrebinofigueroa@alu.frlp.utn.edu.ar>
2024-10-14 18:13:54 -0300

    1

--- react-app-ec/ec/src/pages/Historia.tsx
+++ react-app-ec/ec/src/pages/Historia.tsx
@@ -2,7 +2,6 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Layout from '../components/Layout';
-import ListaExamenes from '../components/ListaExamenes';
 import Warning from '../components/Warning';
 import Alert from '../components/Alerts';
 import fondoGenerico from '../images/fondoGenerico.jpg';
